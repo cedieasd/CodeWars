1d28663a59c8118e9ba9683bdacb3aad7a7a8e27
cedieasd <70503903+cedieasd@users.noreply.github.com>
2022-02-26 20:07:42 +0800

    full push

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+           
+
+        }
+
+        public static string SpinWords(string sentence)
+        {
+            string sentence1 = sentence;
+            string reverse = "";
+
+            for (int i = sentence1.Length - 1; i >= 0; i--)
+            {
+                reverse += sentence1[i];
+            }
+
+            return reverse;
+        }
+
+        
+
+
+
+    }
+}
